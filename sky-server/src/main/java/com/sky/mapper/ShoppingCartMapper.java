8d2534799dbd39a11a76b1f05a895e634a76d9b8
kkstar <give4178@163.com>
2025-08-17 11:21:44 +0800

    删除了多余的引用

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -1,6 +1,6 @@
 package com.sky.mapper;
 
-import com.sky.dto.ShoppingCartDTO;
+
 import com.sky.entity.ShoppingCart;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
